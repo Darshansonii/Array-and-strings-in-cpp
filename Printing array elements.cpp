9f44c1806823cf573c7db8bd472dddb16173b112
Darshansonii <darsan779d.dd@gmail.com>
2025-08-02 07:26:26 -0400

    Add files via upload

---
+++ Printing array elements.cpp
@@ -0,0 +1,21 @@
+//Name-Darshan soni
+//PRN-24070123034
+//Exp-7
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={3,6,3,1,4};
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+
+
+/*
+Output
+3 6 3 1 4
+*/
