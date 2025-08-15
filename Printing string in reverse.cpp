bd0c74063759c16e3087d2b682825d80ae6e7a64
Darshansonii <darsan779d.dd@gmail.com>
2025-08-16 05:17:03 +0530

    Add files via upload

---
+++ Printing string in reverse.cpp
@@ -0,0 +1,27 @@
+//Name-Darshan soni
+//PRN-24070123034
+//Exp-7
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1;
+    string str2;
+    int n;
+    cout << "Enter the string to reverse\n";
+    cin >> str1;
+    n = str1.length();
+    cout << "Reversed String\n";
+    for(int i = n;i>=0;i--){
+        cout << str1[i] ;
+    }
+        return 0;
+    }
+
+/*Output
+Enter the string to reverse
+Darshan
+Reversed String
+nahsraD
+*/
