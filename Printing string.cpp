49ac8b7ea4783d93b792fea53fe89c0c726d4ebe
Darshansonii <darsan779d.dd@gmail.com>
2025-08-16 05:16:03 +0530

    Add files via upload

---
+++ Printing string.cpp
@@ -0,0 +1,27 @@
+//Name-Darshan soni
+//PRN-24070123034
+//Exp-7
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    char str1[4]="C++";
+    char str2[]={'C','+','+','\0'};
+    char str3[4]={'C','+','+','\0'};
+    string str4="C++";
+    
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+    cout<<str3<<endl;
+    cout<<str4<<endl;
+    return 0;
+}
+
+/*Output
+C++
+C++
+C++
+C++
+*/
