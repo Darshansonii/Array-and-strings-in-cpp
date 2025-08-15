112c0ec5f55ef312017d5762fb653dc6ab6d3c4d
Darshansonii <darsan779d.dd@gmail.com>
2025-08-16 05:16:39 +0530

    Add files via upload

---
+++ Concatenation of string.cpp
@@ -0,0 +1,20 @@
+//Name-Darshan soni
+//PRN-24070123034
+//Exp-7
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str1="Darshan Soni- ";
+    string str2="24070123034";
+    
+    cout<<str1+str2;
+    
+    return 0;
+}
+
+/*Output
+Darshan Soni- 24070123034
+*/
